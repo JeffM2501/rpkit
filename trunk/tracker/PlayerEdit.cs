a24acd48a0734a185d0f8961d22bb782c8147f58
JeffM2501 <JeffM2501@57cb1743-0352-0410-b2bf-f108194d427c>
2008-08-23 19:23:49 +0000

    store data in the documents dir. start a player editor dialog

---
+++ trunk/tracker/PlayerEdit.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace tracker
+{
+    public partial class PlayerEdit : Form
+    {
+        public PlayerEdit()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
