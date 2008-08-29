cc7bd09b99ec63c4453c5186f875a40fd9849d48
JeffM2501 <JeffM2501@57cb1743-0352-0410-b2bf-f108194d427c>
2008-08-29 02:26:41 +0000

    start user management and forms

--- server/server.cs
+++ server/server.cs
@@ -2,16 +2,99 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Net;
+using System.IO;
+using System.Net.Mail;
+using System.Security.Cryptography;
 
+using RPUsers;
 
 namespace RPServer
 {
+    public class Connection 
+    {
+        public Connection ( HttpListenerContext c )
+        {
+            context = c;
+
+            HttpListenerRequest request = context.Request;
+
+            arguments.Clear();
+
+            // get our base keys
+            if(request.QueryString.HasKeys())
+            {
+                for (int i = 0; i < request.QueryString.Count; i++ )
+                {
+                    string name =  request.QueryString.GetKey(i);
+                    string value = string.Empty;
+
+                    if (request.QueryString.GetValues(i).GetLength(0) > 0)
+                        value = request.QueryString.GetValues(i)[0];
+
+                    arguments.Add(name,value);
+                }
+             }
+        }
+
+        public string getArg ( string key )
+        {
+            if (arguments.ContainsKey(key))
+                return arguments[key];
+            return null;
+        }
+
+        public HttpListenerContext context;
+        Dictionary<string,string>   arguments = new Dictionary<string,string>();
+    }
+
     public class Server
     {
         List<string> authedSessions = new List<string>();
-        public void init ( )
+
+        Usermanager users = new Usermanager();
+
+        public string publicURL = "http://localhost";
+        public string replyEMail = "nobody@host.com";
+
+        private void writeToContext ( string text, HttpListenerContext context )
+        {
+            context.Response.OutputStream.Write(System.Text.ASCIIEncoding.ASCII.GetBytes(text), 0, text.Length);
+        }
+
+        private void writeToContext ( string text, Connection connection )
+        {
+            writeToContext(text,connection.context);
+        }
+
+        private void writeHTTPHeader ( Connection connection )
+        {
+            connection.context.Response.ContentType = "text/html";
+            writeToContext("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">", connection.context);
+            writeToContext("<html><head>", connection.context);
+            writeToContext("</head><body>", connection.context);
+        }
+
+        private void writeHTTPHeader(Connection connection, string title)
+        {
+            connection.context.Response.ContentType = "text/html";
+            writeToContext("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">", connection.context);
+            writeToContext("<html><head>", connection.context);
+            if (title.Length > 0)
+                writeToContext("<title>" + title + "</title", connection.context);
+            writeToContext("</head><body>", connection.context);
+        }
+
+        private void writeHTTPFooter(Connection connection)
+        {
+            writeToContext("</body></html>", connection.context);
+        }
+
+        public void init ( string host )
         {
             // load all the databases
+            publicURL = host;
+
+            users.load(new DirectoryInfo("./users/"));
         }
 
         public void update ( )
@@ -19,13 +102,121 @@ namespace RPServer
 
         }
 
-        private void newUser ( HttpListenerContext context )
+        private bool fieldValid ( string text )
         {
-
+            if (text != null || text.Length == 0)
+                return false;
+            return true;
         }
 
-        private void login( HttpListenerContext context, string session, string action )
+        private void newUser ( Connection connection )
         {
+            writeHTTPHeader(connection);
+
+            string mode = connection.getArg("mode");
+            if ( mode == "adduser")
+            {
+                string username = connection.getArg("username");
+                string password = connection.getArg("password");
+                string password2 = connection.getArg("password2");
+                string email = connection.getArg("email");
+                string email2 = connection.getArg("email2");
+
+                // verify the info
+                if (!fieldValid(username) || !fieldValid(password) || fieldValid(email) )
+                {
+                    writeToContext("<h2>Required info was missing!</h2>",connection);
+                    return;
+                }
+
+                if (users.getUser(username) != null)
+                {
+                    writeToContext("<h2>Username is taken!</h2>",connection);
+                    return;
+                }
+
+                if ( password != password2)
+                {
+                    writeToContext("<h2>Passwords do not match</h2>",connection);
+                    return;
+                }
+
+                if ( password.Length < 6)
+                {
+                    writeToContext("<h2>Passwords too short</h2>",connection);
+                    return;
+                }
+
+                if ( !email.Contains("@") || !email.Contains("."))
+                {
+                    writeToContext("<h2>Invalid E-mail</h2>",connection);
+                    return;
+                }
+
+                if ( email != email2)
+                {
+                    writeToContext("<h2>E-Mail does not match</h2>",connection);
+                    return;
+                }
+
+                // all is good, make a user and have them verify
+
+                Random rng = new Random();
+
+                string verifyKey = string.Empty;
+
+                for ( int i = 0; i < 5; i++)
+                    verifyKey += rng.Next().ToString();
+
+                SmtpClient mailer = new SmtpClient();
+                string subject = "Registration with " + publicURL;
+                string body = "Thanks for registering, click this link to verify your address\n";
+                body += publicURL + "?action=adduser&mode=verify&key=" + verifyKey;
+                body += "\n<a href=\"" + publicURL + "?action=adduser&mode=verify&key=" + verifyKey + "\">Here</a>";
+
+                mailer.Send(replyEMail, email, subject, body);
+
+                writeToContext("<h2>Thank you for registering, you have been sent a confirmation e-mail, click the link to verify your account</h2>",connection);
+
+                User user = new User();
+                user.email = email;
+                user.username = username;
+                user.verified = false;
+                user.emailKey = verifyKey;
+
+                MD5 md5 = MD5.Create();
+                md5.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(password));
+
+                user.passwordHash = md5.GetHashCode().ToString();;
+            }
+            else if ( mode == "verify" )
+            {
+                // this has to come form the verifyer
+            }
+            else // new user form
+            {
+                writeToContext("</br><h2>Please fill out the following information</h2",connection);
+                writeToContext("<form name=\"input\" action=\"?\" method=\"put\">",connection);
+
+                writeToContext("<input type=\"hidden\" name=\"action\" value=\"newuser\">",connection);
+                writeToContext("<input type=\"hidden\" name=\"mode\" value=\"adduser\">",connection);
+                writeToContext("</br>Username<input type=\"text\" name=\"username\">",connection);
+                writeToContext("</br>E-mail<input type=\"text\" name=\"email\">",connection);
+                writeToContext("</br>E-mail(confirm)<input type=\"text\" name=\"email2\">",connection);
+                writeToContext("</br>Password (6 character min)<input password=\"text\" name=\"password\">",connection);
+                writeToContext("</br>Password(confirm)<input password=\"text\" name=\"password2\">",connection);
+                writeToContext("</form></br>", connection);
+            }
+
+            writeHTTPFooter(connection);
+        }
+
+        private void login( Connection connection, string session )
+        {
+            writeHTTPHeader(connection);
+
+            string action = connection.getArg("action");
+
             if (action == "login")
             {
                 // auth
@@ -33,26 +224,36 @@ namespace RPServer
             else
             {
                 //login page
+                writeToContext("</br><h2>Please Login</h2",connection);
+                writeToContext("<form name=\"input\" action=\"?\" method=\"put\">",connection);
+
+                writeToContext("<input type=\"hidden\" name=\"action\" value=\"login\">",connection);
+                writeToContext("</br>Username<input type=\"text\" name=\"username\">",connection);
+                writeToContext("</br>Password<input password=\"text\" name=\"password\">",connection);
+                writeToContext("</form></br>", connection);
+                writeToContext("<a href=\"?action=newuser\">New User?</a>",connection);
             }
+            writeHTTPFooter(connection);
         }
 
-        private bool handleAuthedQuery(HttpListenerContext context, string session, string action)
+        private bool handleAuthedQuery( Connection connection, string session )
         {
-            string message;
+            writeHTTPHeader(connection);
+
+            string action = connection.getArg("action");
 
             if (action == "stop")
             {
-                message = "ok, shutdown!";
-                context.Response.OutputStream.Write(System.Text.ASCIIEncoding.ASCII.GetBytes(message), 0, message.Length);
+                writeToContext("ok, shutdown!", connection);
+                writeHTTPFooter(connection);
                 return true;
             }
 
-            message = "Hello World!";
-            context.Response.OutputStream.Write(System.Text.ASCIIEncoding.ASCII.GetBytes(message), 0, message.Length);
+            writeToContext("Hello World!", connection);
+            writeHTTPFooter(connection);
             return false;
         }
-
-
+        
         private bool isAuthedSession ( string session )
         {
             if (session == string.Empty)
@@ -61,7 +262,7 @@ namespace RPServer
             return authedSessions.Contains(session);
         }
 
-        private bool handleAppQuery ( HttpListenerContext context )
+        private bool handleAppQuery ( Connection connection )
         {
             // parse out the message in a nice fast XML way
             return false;
@@ -69,32 +270,19 @@ namespace RPServer
 
         public bool handleURL ( HttpListenerContext context, Uri relitiveUri )
         {
-            // grab the cookie
+            Connection connection = new Connection(context);
+
             string session = string.Empty;
-            string action = string.Empty;
 
-            HttpListenerRequest request = context.Request;
+            string action = connection.getArg("action");
+            if (connection.getArg("type") == "rpkit")
+                return handleAppQuery(connection);
 
-            // get our base keys
-            if(request.QueryString.HasKeys())
-            {
-                for (int i = 0; i < request.QueryString.Count; i++ )
-                {
-                    string name =  request.QueryString.GetKey(i);
-                    if (name == "type")
-                    {
-                        if (request.QueryString.GetValues(i)[0] == "rpkit")
-                            return handleAppQuery(context);
-                    }
-                    else if ( name == "action")
-                        action = request.QueryString.GetValues(i)[0];
-                }
-             }
 
             // new user dosnt' need a session
             if (action == "newuser")
             {
-                newUser(context);
+                newUser(connection);
                 return false;
             }
             
@@ -108,11 +296,11 @@ namespace RPServer
             // if there is no session, 
             if (!isAuthedSession(session))
             {
-                login(context,session,action);
+                login(connection,session);
                 return false;
             }
 
-            return handleAuthedQuery(context, session, action);
+            return handleAuthedQuery(connection, session);
         }
     }
 }
